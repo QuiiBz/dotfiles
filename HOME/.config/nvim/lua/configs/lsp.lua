5b9db5ceab78c34a6d689179498f05f1c6aea1cb
QuiiBz <tom.lienrd@gmail.com>
2021-08-30 17:14:28 +0200

    refactor(nvim): Update lsp

--- HOME/.config/nvim/lua/configs/lsp.lua
+++ HOME/.config/nvim/lua/configs/lsp.lua
@@ -78,93 +78,83 @@ local on_attach = function(client, bufnr)
   })
 end
 
-local function setup_lsp()
-  require'lspinstall'.setup()
-  require("null-ls").config {}
-  require("lspconfig")["null-ls"].setup {}
+require'lspinstall'.setup()
+require("null-ls").config {}
+require("lspconfig")["null-ls"].setup {}
 
-  local servers = {
-    'vimls',
-    'sumneko_lua',
-    'tsserver',
-    'rust_analyzer',
-    'java_language_server',
-    'cssls',
-    'jsonls',
-    'html'
-  }
+local servers = {
+  'vimls',
+  'sumneko_lua',
+  'tsserver',
+  'rust_analyzer',
+  'java_language_server',
+  'cssls',
+  'jsonls',
+  'html'
+}
 
-  local config_path = vim.fn.stdpath('config')
+local config_path = vim.fn.stdpath('config')
 
-  for _, server in pairs(servers) do
-    local config = { on_attach = on_attach }
+for _, server in pairs(servers) do
+  local config = { on_attach = on_attach }
 
-    if server == 'java_language_server' then
-      config.cmd = { config_path.."/java-language-server/dist/lang_server_mac.sh" }
-      config.filetypes = { 'java' }
-      config.settings = {  }
-    end
-
-    if server == 'sumneko_lua' then
-      local system_name
-      if vim.fn.has("mac") == 1 then
-        system_name = "macOS"
-      elseif vim.fn.has("unix") == 1 then
-        system_name = "Linux"
-      elseif vim.fn.has('win32') == 1 then
-        system_name = "Windows"
-      else
-        print("Unsupported system for sumneko")
-      end
-
-      -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-      local sumneko_root_path = config_path..'/lua-language-server'
-      local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
-
-      local runtime_path = vim.split(package.path, ';')
-      table.insert(runtime_path, "lua/?.lua")
-      table.insert(runtime_path, "lua/?/init.lua")
-
-      config.cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" };
-      config.settings = {
-        Lua = {
-          runtime = {
-            -- LuaJIT in the case of Neovim
-            version = 'LuaJIT',
-            path = vim.split(package.path, ';'),
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = {
-              [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-              [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-            },
-          },
-        }
-      }
-    end
-
-    if server == 'cssls' or server == 'jsonls' or server == 'html' then
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-      config.capabilities = capabilities
-    end
-
-    require'lspconfig'[server].setup(config)
+  if server == 'java_language_server' then
+    config.cmd = { config_path.."/java-language-server/dist/lang_server_mac.sh" }
+    config.filetypes = { 'java' }
+    config.settings = {  }
   end
-end
 
-setup_lsp()
+  if server == 'sumneko_lua' then
+    local system_name
+    if vim.fn.has("mac") == 1 then
+      system_name = "macOS"
+    elseif vim.fn.has("unix") == 1 then
+      system_name = "Linux"
+    elseif vim.fn.has('win32') == 1 then
+      system_name = "Windows"
+    else
+      print("Unsupported system for sumneko")
+    end
 
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
-require'lspinstall'.post_install_hook = function ()
-  setup_lsp() -- reload installed servers
-  vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+    -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+    local sumneko_root_path = config_path..'/lua-language-server'
+    local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+
+    local runtime_path = vim.split(package.path, ';')
+    table.insert(runtime_path, "lua/?.lua")
+    table.insert(runtime_path, "lua/?/init.lua")
+
+    config.cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" };
+    config.settings = {
+      Lua = {
+        runtime = {
+          -- LuaJIT in the case of Neovim
+          version = 'LuaJIT',
+          path = vim.split(package.path, ';'),
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          },
+        },
+      }
+    }
+  end
+
+  if server == 'cssls' or server == 'jsonls' or server == 'html' then
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+    config.capabilities = capabilities
+  end
+
+  require'lspconfig'[server].setup(config)
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
