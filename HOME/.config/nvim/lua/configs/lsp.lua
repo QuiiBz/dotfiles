9b9b6ab0c866c3207ba45a2d10e438140cc456d5
QuiiBz <tom.lienrd@gmail.com>
2021-09-04 17:27:43 +0200

    refactor(nvim): Async formatting

--- HOME/.config/nvim/lua/configs/lsp.lua
+++ HOME/.config/nvim/lua/configs/lsp.lua
@@ -21,7 +21,10 @@ local on_attach = function(client, bufnr)
   -- vim.cmd("command -buffer FormattingSync lua vim.lsp.buf.formatting_sync()")
 
   -- format on save
-  vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+  vim.api.nvim_command [[augroup Format]]
+  vim.api.nvim_command [[autocmd! * <buffer>]]
+  vim.api.nvim_command [[autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()]]
+  vim.api.nvim_command [[augroup END]]
 
   local ts_utils = require("nvim-lsp-ts-utils")
 
@@ -174,6 +177,22 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
   border = "single"
 })
+
+vim.lsp.handlers["textDocument/formatting"] = function(err, _, result, _, bufnr)
+  if err ~= nil or result == nil then
+    return
+  end
+
+  if not vim.api.nvim_buf_get_option(bufnr, "modified") then
+    local view = vim.fn.winsaveview()
+    vim.lsp.util.apply_text_edits(result, bufnr)
+    vim.fn.winrestview(view)
+
+    if bufnr == vim.api.nvim_get_current_buf() then
+      vim.api.nvim_command("noautocmd :update")
+    end
+  end
+end
 
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 
