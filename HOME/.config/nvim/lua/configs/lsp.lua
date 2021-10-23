067d572f68791e988122c8394b1e9386d557475e
QuiiBz <tom.lienrd@gmail.com>
2021-10-23 09:17:22 +0200

    feat(nvim): Set package manager to yarn for eslint

--- HOME/.config/nvim/lua/configs/lsp.lua
+++ HOME/.config/nvim/lua/configs/lsp.lua
@@ -107,6 +107,12 @@ for _, server in pairs(servers) do
     config.capabilities = capabilities
   end
 
+  if server == 'eslint' then
+    config.settings = {
+      packageManager = 'yarn',
+    }
+  end
+
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   config.capabilities = capabilities
