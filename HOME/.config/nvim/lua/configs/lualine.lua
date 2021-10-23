cde794e9d021f2aadf388e686af4fd22a751dfaa
QuiiBz <tom.lienrd@gmail.com>
2021-10-23 09:19:37 +0200

    fix(nvim): Update lualine separators

--- HOME/.config/nvim/lua/configs/lualine.lua
+++ HOME/.config/nvim/lua/configs/lualine.lua
@@ -2,8 +2,8 @@ require'lualine'.setup {
   options = {
     theme = 'tokyonight',
     -- theme = 'github',
-    section_separators = {'', ''},
-    component_separators = {'', ''},
+    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
     disabled_filetypes = { 'NvimTree' }
   }
 }
