cac7341c38f5dd893078a56585d28ea1869fd827
QuiiBz <tlienard@scaleway.com>
2023-02-16 12:10:00 +0100

    feat(nvim): add eslint code actions

--- HOME/.config/nvim/lua/plugins/lsp.lua
+++ HOME/.config/nvim/lua/plugins/lsp.lua
@@ -68,6 +68,7 @@ require('null-ls').setup({
     null_ls.builtins.formatting.prismaFmt,
     null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
   },
   on_attach = function(client, bufnr)
     if client.supports_method('textDocument/formatting') then
