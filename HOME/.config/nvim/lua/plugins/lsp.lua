12b1b167c8735d1b64d8227946ae4c020d810bc7
QuiiBz <tom.lienrd@gmail.com>
2023-07-20 19:51:43 +0200

    fix(nvim): use eslint_d for formatting

--- HOME/.config/nvim/lua/plugins/lsp.lua
+++ HOME/.config/nvim/lua/plugins/lsp.lua
@@ -66,7 +66,7 @@ local null_ls = require('null-ls')
 -- Setup formatting and diagnostics with null-ls
 require('null-ls').setup({
   sources = {
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.eslint_d,
     null_ls.builtins.formatting.prismaFmt,
     null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.diagnostics.eslint_d,
