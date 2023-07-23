2473e37981ae1b5863577998d417462196b464e4
QuiiBz <tom.lienrd@gmail.com>
2023-07-23 17:16:47 +0200

    fix(nvim): use git_files instead of find_files for telescope

--- HOME/.config/nvim/lua/plugins/telescope.lua
+++ HOME/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
 
 require('telescope').setup{
