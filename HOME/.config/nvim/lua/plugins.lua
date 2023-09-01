13fdad3953540757fb0a5806d82ebe4b7e031f42
QuiiBz <tom.lienrd@gmail.com>
2023-09-01 13:37:39 +0200

    feat(nvim): better code actions

--- HOME/.config/nvim/lua/plugins.lua
+++ HOME/.config/nvim/lua/plugins.lua
@@ -63,4 +63,15 @@ return {
     keys = { '<leader>S' },
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
+  {
+    'weilbith/nvim-code-action-menu',
+    keys = {
+      { 'ga', '<cmd>CodeActionMenu<cr>' }
+    },
+    init = function()
+      vim.g.code_action_menu_window_border = 'rounded'
+      vim.g.code_action_menu_show_action_kind = false
+      vim.g.code_action_menu_show_details = false
+    end
+  }
 }
