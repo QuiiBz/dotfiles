4b1a954b3db0ceb8d1daae1f4c8f99ab083e0e90
QuiiBz <tom.lienrd@gmail.com>
2023-11-19 16:20:44 +0100

    fix(nvim): hint diagnostics icon

--- HOME/.config/nvim/lua/settings.lua
+++ HOME/.config/nvim/lua/settings.lua
@@ -32,7 +32,7 @@ vim.fn.sign_define('DiagnosticSignWarn',
 vim.fn.sign_define('DiagnosticSignInfo',
   { text = ' ', texthl = 'DiagnosticSignInfo' })
 vim.fn.sign_define('DiagnosticSignHint',
-  { text = '', texthl = 'DiagnosticSignHint' })
+  { text = '', texthl = 'DiagnosticSignHint' })
 
 vim.g.skip_ts_context_commentstring_module = true
 
