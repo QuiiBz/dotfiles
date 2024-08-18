ad14afc56925e32c2e02e46c93b916ad2e1b6453
Tom Lienard <tom.lienrd@gmail.com>
2024-08-18 09:48:02 +0100

    feat(nvim): try mini.files to replace nvim-tree

--- HOME/.config/nvim/lua/plugins/tree.lua
+++ HOME/.config/nvim/lua/plugins/tree.lua
@@ -2,9 +2,9 @@ return {
   {
     'nvim-tree/nvim-tree.lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = {
-      { '<C-n>', '<cmd>NvimTreeToggle<cr>' }
-    },
+    -- keys = {
+    --   { '<C-n>', '<cmd>NvimTreeToggle<cr>' }
+    -- },
     config = function()
       require('nvim-tree').setup({
         view = {
@@ -36,5 +36,39 @@ return {
         },
       })
     end
-  }
+  },
+  {
+    'echasnovski/mini.files',
+    version = '*',
+    keys = {
+      { '<C-n>', '<cmd>lua MiniFiles.open()<cr>' }
+    },
+    config = function()
+      require('mini.files').setup({
+        mappings = {
+          go_in = '<Tab>',
+          go_in_plus = '<Enter>',
+          go_out = '<Esc>',
+        },
+        options = {
+          permanent_delete = false,
+        },
+        windows = {
+          preview = true,
+          width_focus = 25,
+          width_preview = 50,
+        },
+      })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesWindowOpen',
+        callback = function(args)
+          local win_id = args.data.win_id
+          local config = vim.api.nvim_win_get_config(win_id)
+          config.border = 'rounded'
+          vim.api.nvim_win_set_config(win_id, config)
+        end,
+      })
+    end
+  },
 }
