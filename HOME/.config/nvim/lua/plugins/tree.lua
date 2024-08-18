c070742f90c8c728a51cc437561c1570d90f49ed
Tom Lienard <tom.lienrd@gmail.com>
2024-08-18 09:59:17 +0100

    feat(nvim): hide dotfiles in mini.files by default

--- HOME/.config/nvim/lua/plugins/tree.lua
+++ HOME/.config/nvim/lua/plugins/tree.lua
@@ -69,6 +69,25 @@ return {
           vim.api.nvim_win_set_config(win_id, config)
         end,
       })
+
+      local show_dotfiles = false
+      local filter_show = function(fs_entry) return true end
+      local filter_hide = function(fs_entry)
+        return not vim.startswith(fs_entry.name, '.')
+      end
+      local toggle_dotfiles = function()
+        show_dotfiles = not show_dotfiles
+        local new_filter = show_dotfiles and filter_show or filter_hide
+        MiniFiles.refresh({ content = { filter = new_filter } })
+      end
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          vim.keymap.set('n', '.', toggle_dotfiles, { buffer = buf_id })
+        end,
+      })
     end
   },
 }
