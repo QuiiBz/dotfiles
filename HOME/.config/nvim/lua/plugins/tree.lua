a277bffd644f8dc5e409f107632f35785092a137
Tom Lienard <tom.lienrd@gmail.com>
2024-08-18 13:53:16 +0100

    feat(nvim): hidden dotfiles by default & remember on re-open

--- HOME/.config/nvim/lua/plugins/tree.lua
+++ HOME/.config/nvim/lua/plugins/tree.lua
@@ -41,9 +41,22 @@ return {
     'echasnovski/mini.files',
     version = '*',
     keys = {
-      { '<C-n>', '<cmd>lua MiniFiles.open()<cr>' }
+      { '<C-n>', '<cmd>lua if not MiniFiles.close() then MiniFiles.open() end<cr>' }
     },
     config = function()
+      local show_dotfiles = false
+      local filter_show = function()
+        return true
+      end
+      local filter_hide = function(fs_entry)
+        return not vim.startswith(fs_entry.name, '.')
+      end
+      local toggle_dotfiles = function()
+        show_dotfiles = not show_dotfiles
+        local new_filter = show_dotfiles and filter_show or filter_hide
+        MiniFiles.refresh({ content = { filter = new_filter } })
+      end
+
       require('mini.files').setup({
         mappings = {
           go_in = '<Tab>',
@@ -60,6 +73,25 @@ return {
         },
       })
 
+      --- Use '.' to toggle dotfiles visibility (off by default)
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          vim.keymap.set('n', '.', toggle_dotfiles, { buffer = buf_id })
+        end,
+      })
+
+      -- Remember if we wanted to hide/show dotfiles after re-opening
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesExplorerOpen',
+        callback = function()
+          local new_filter = show_dotfiles and filter_show or filter_hide
+          MiniFiles.refresh({ content = { filter = new_filter } })
+        end,
+      })
+
+      --- Rounded borders
       vim.api.nvim_create_autocmd('User', {
         pattern = 'MiniFilesWindowOpen',
         callback = function(args)
@@ -67,25 +99,6 @@ return {
           local config = vim.api.nvim_win_get_config(win_id)
           config.border = 'rounded'
           vim.api.nvim_win_set_config(win_id, config)
-        end,
-      })
-
-      local show_dotfiles = false
-      local filter_show = function(fs_entry) return true end
-      local filter_hide = function(fs_entry)
-        return not vim.startswith(fs_entry.name, '.')
-      end
-      local toggle_dotfiles = function()
-        show_dotfiles = not show_dotfiles
-        local new_filter = show_dotfiles and filter_show or filter_hide
-        MiniFiles.refresh({ content = { filter = new_filter } })
-      end
-
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'MiniFilesBufferCreate',
-        callback = function(args)
-          local buf_id = args.data.buf_id
-          vim.keymap.set('n', '.', toggle_dotfiles, { buffer = buf_id })
         end,
       })
     end
