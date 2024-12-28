6dd77ae8c7208ee2f427c2ad581ea1aa400f0f7a
Tom Lienard <tom.lienrd@gmail.com>
2024-12-28 17:13:07 +0100

    feat(nvim): more minimal bufferline config

--- HOME/.config/nvim/lua/plugins/bufferline.lua
+++ HOME/.config/nvim/lua/plugins/bufferline.lua
@@ -4,8 +4,23 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     after = 'catppuccin',
     config = function()
+      local C = require('catppuccin.palettes').get_palette()
+
       require('bufferline').setup({
-        highlights = require('catppuccin.groups.integrations.bufferline').get(),
+        highlights = require('catppuccin.groups.integrations.bufferline').get({
+          custom = {
+            all = {
+              -- Overwrite the background colors to be the same as all backgrounds
+              fill = { bg = C.base },
+              background = { bg = C.base },
+              buffer_visible = { bg = C.base },
+              indicator_visible = { bg = C.base },
+              duplicate = { bg = C.base },
+              duplicate_visible = { bg = C.base },
+              duplicate_selected = { bg = C.base },
+            },
+          },
+        }),
         options = {
           show_buffer_close_icons = false,
           show_close_icon = false,
