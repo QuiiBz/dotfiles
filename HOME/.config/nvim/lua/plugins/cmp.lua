2b477083398d83f8d73ad43ce808d9ee5fd245a4
Tom Lienard <tom.lienrd@gmail.com>
2025-02-16 10:56:06 +0000

    feat(nvim): update plugins

--- HOME/.config/nvim/lua/plugins/cmp.lua
+++ HOME/.config/nvim/lua/plugins/cmp.lua
@@ -14,9 +14,11 @@ return {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
       },
+      cmdline = {
+        sources = {},
+      },
       sources = {
         default = { 'lsp', 'buffer', 'path' },
-        cmdline = {},
       },
       completion = {
         menu = {
