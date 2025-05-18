b10e3194b177a0a494b3c4a0b7c7f8b3ade2839d
Tom Lienard <tom.lienrd@gmail.com>
2025-05-18 08:54:54 +0100

    feat(nvim): improve bufferline style

--- HOME/.config/nvim/lua/plugins/bufferline.lua
+++ HOME/.config/nvim/lua/plugins/bufferline.lua
@@ -18,14 +18,22 @@ return {
               duplicate = { bg = C.base },
               duplicate_visible = { bg = C.base },
               duplicate_selected = { bg = C.base },
+              info = { bg = C.base },
+              warning = { bg = C.base },
+              error = { bg = C.base },
             },
           },
         }),
         options = {
+          indicator = {
+            style = 'none',
+          },
           show_buffer_close_icons = false,
+          show_tab_indicators = false,
           show_close_icon = false,
           separator_style = { '', '' },
           diagnostics = 'nvim_lsp',
+          sort_by = 'insert_after_current',
         },
       })
     end
