7d75afe460fca491bd3489ffd26762f017c9eac5
Tom Lienard <tom.lienrd@gmail.com>
2025-06-15 10:06:26 +0100

    feat(nvim): update avante version

--- HOME/.config/nvim/lua/plugins/ai.lua
+++ HOME/.config/nvim/lua/plugins/ai.lua
@@ -26,23 +26,32 @@ return {
   },
   {
     'yetone/avante.nvim',
-    cmd = 'AvanteChat',
     keys = {
-      { '<leader>aa', '<cmd>AvanteChat<cr>' }
+      { '<leader>aa', '<cmd>AvanteChat<cr>' },
+      { '<leader>ae', '<cmd>AvanteEdit<cr>', mode = 'v' },
     },
-    version = 'v0.0.23',
+    version = 'v0.0.25',
     opts = {
-      provider = 'copilot',
-      copilot = {
-        model = 'gpt-4.1',
-        max_tokens = 120000, -- max tokens for GPT-4.1 is 128k, so add some buffer
+      providers = {
+        copilot = {
+          model = 'gpt-4.1',
+          extra_request_body = {
+            max_tokens = 120000, -- max tokens for GPT-4.1 is 128k, so add some buffer
+          },
+        },
       },
+      provider = 'copilot',
       auto_suggestions_provider = nil,
       behaviour = {
         auto_suggestions = false,
       },
       hints = {
         enabled = false,
+      },
+      windows = {
+        edit = {
+          border = 'rounded',
+        },
       },
     },
     build = 'make',
