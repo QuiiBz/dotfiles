8d8f1e42af33f0339c1b8f58e71f019debadc79c
Tom Lienard <tom.lienrd@gmail.com>
2025-09-29 10:53:35 +0100

    fix(nvim): update bufferline catppuccin theme

--- HOME/.config/nvim/lua/plugins/bufferline.lua
+++ HOME/.config/nvim/lua/plugins/bufferline.lua
@@ -5,7 +5,7 @@ return {
     after = 'catppuccin',
     config = function()
       require('bufferline').setup({
-        highlights = require('catppuccin.groups.integrations.bufferline').get_theme(),
+        highlights = require('catppuccin.special.bufferline').get_theme(),
         options = {
           indicator = {
             style = 'none',
