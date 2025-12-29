aa7658f13b183e233707bc5ae48d273ca5ef54c1
Tom Lienard <tom.lienrd@gmail.com>
2025-12-29 11:49:57 +0100

    feat(aerospace): use window animations

--- HOME/.config/aerospace/sticky-window.sh
+++ HOME/.config/aerospace/sticky-window.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # https://github.com/nikitabobko/AeroSpace/issues/2#issuecomment-3023036984
-current_workspace=$(aerospace list-workspaces --focused)
-aerospace list-windows --all | grep -E "(AI Chat)" | awk '{print $1}' | while read window_id; do
+current_workspace=$(/usr/local/bin/aerospace list-workspaces --focused)
+/usr/local/bin/aerospace list-windows --all | grep -E "(AI Chat)" | awk '{print $1}' | while read window_id; do
     if [ -n "$window_id" ]; then
-        aerospace move-node-to-workspace --window-id "$window_id" "$current_workspace"
+        /usr/local/bin/aerospace move-node-to-workspace --window-id "$window_id" "$current_workspace"
     fi
 done
