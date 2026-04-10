5930108a3c26fdba3868c78238c0738bebf446f8
Tom Lienard <tom.lienrd@gmail.com>
2026-04-10 18:55:34 +0100

    fix(nvim): correct bg color for selected buffers

--- HOME/.config/nvim/lua/plugins/bufferline.lua
+++ HOME/.config/nvim/lua/plugins/bufferline.lua
@@ -5,15 +5,27 @@ return {
     after = 'catppuccin',
     config = function()
       local bufferline = require('bufferline')
+      local selected_bg = '#34364B'
+      local accent_fg = '#c6a0f6'
       bufferline.setup({
         highlights = require('catppuccin.special.bufferline').get_theme({
           styles = { 'bold' },
           custom = {
             all = {
-              buffer_selected = { bg = '#34364B' },
-              modified = { fg = '#c6a0f6' },
-              modified_selected = { fg = '#c6a0f6' },
-              indicator_selected = { fg = '#c6a0f6' },
+              buffer_selected = { bg = selected_bg },
+              diagnostic_selected = { bg = selected_bg },
+              error_selected = { bg = selected_bg },
+              warning_selected = { bg = selected_bg },
+              info_selected = { bg = selected_bg },
+              hint_selected = { bg = selected_bg },
+              error_diagnostic_selected = { bg = selected_bg },
+              warning_diagnostic_selected = { bg = selected_bg },
+              info_diagnostic_selected = { bg = selected_bg },
+              hint_diagnostic_selected = { bg = selected_bg },
+              modified = { fg = accent_fg },
+              modified_selected = { fg = accent_fg, bg = selected_bg },
+              close_button_selected = { bg = selected_bg },
+              indicator_selected = { fg = accent_fg, bg = selected_bg },
             },
           },
         }),
