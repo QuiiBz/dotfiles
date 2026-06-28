83d077ea19b14a35f1d8b8fe73e5e72f9cbb2dd0
Tom Lienard <tom.lienrd@gmail.com>
2026-06-28 15:25:31 +0100

    feat(nvim): simplify lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,32 +5,33 @@ return {
     config = function()
       require('lualine').setup({
         options = {
-          component_separators = '',
-          section_separators = { left = '', right = '' },
+          component_separators = '  ',
           globalstatus = true,
           theme = 'catppuccin-nvim',
         },
         sections = {
-          lualine_a = { 'mode' },
+          lualine_a = {},
           lualine_b = {},
           lualine_c = {
             {
               'filename',
+              file_status = false,
               path = 1,
             },
           },
-          lualine_x = { 'searchcount' },
-          lualine_y = {},
-          lualine_z = {
+          lualine_x = {
+            'searchcount',
             -- Show macro recording status
             function()
               local reg = vim.fn.reg_recording()
               if reg == '' then
                 return ''
               end
-              return '󰵆  ' .. reg
+              return '@' .. reg
             end,
           },
+          lualine_y = {},
+          lualine_z = {},
         },
         inactive_sections = {},
         tabline = {},
